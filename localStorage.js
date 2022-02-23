53383f195b7460bd79a46551bb0b6b44a3f6d75a
Gilmar Silva Santos Junior <gilmar.santos@jchelby.com>
2022-02-23 01:44:38 -0300

    change variable name to a clean name

--- localStorage.js
+++ localStorage.js
@@ -1,17 +1,16 @@
-function getApi(endpoint) {
-    var x = endpoint;
-    var local = localStorage.getItem(x);
-    localStorage.clear()
+function getApi(param) {
+
+    var local = localStorage.getItem(param);
     //Se existir localStorage do cep indicado, consome através dele.
     if (local) {
         console.log(JSON.parse(local));
         console.log('A api foi consumida por localStorage e reduziu o consumo de dados e processamento.')
     } else {
         //Se não existir localStorage, consome normalmente cria um localstorage.
-        fetch('https://viacep.com.br/ws/' + x + '/json/')
+        fetch('https://viacep.com.br/ws/' + param + '/json/')
             .then(response => response.json())
             .then(function (e) {
-                localStorage.setItem(x, JSON.stringify(e))
+                localStorage.setItem(param, JSON.stringify(e))
                 console.log(e);
                 console.log('A api foi consumida e foi gerado um cookie para armazenar este cep.')
 
